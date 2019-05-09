be6fc254f83f3818c865593b9ced9346ad34c0cf
Jacky Zhao <jacky12311@gmail.com>
2019-05-08 22:56:13 -0400

    added border, position teleport

--- Snakegame/GameScene.swift
+++ Snakegame/GameScene.swift
@@ -14,6 +14,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var apple = SKSpriteNode()
     var snakeSections: [SKSpriteNode] = []
     
+    var hasContacted = false
+    
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self
         apple = self.childNode(withName: "Apple") as! SKSpriteNode
@@ -21,38 +23,46 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
         initSwipeGestures(view)
         
-        for _ in 0...5 {
-            let newSnake = (snakeSections[0].copy() as! SKSpriteNode)
-            newSnake.position = CGPoint(x: snakeSections[0].position.x, y: snakeSections.last!.position.y - 50)
-            
-            newSnake.color = .green
-            
-            snakeSections.append(newSnake)
-            self.addChild(newSnake)
-        }
+//        for _ in 0...5 {
+//            let newSnake = (snakeSections[0].copy() as! SKSpriteNode)
+//            newSnake.position = CGPoint(x: snakeSections[0].position.x, y: snakeSections.last!.position.y - 50)
+//
+//            newSnake.color = .green
+//
+//            snakeSections.append(newSnake)
+//            self.addChild(newSnake)
+//        }
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
-        border.categoryBitMask = 2
+        border.categoryBitMask = 7 // 0111
         border.isDynamic = false
-        border.affectedByGravity = false
         self.physicsBody = border
         
-        snakeSections[0].physicsBody?.categoryBitMask = 4
-        snakeSections[0].physicsBody?.contactTestBitMask = 2
+        snakeSections[0].physicsBody?.categoryBitMask = 1 // 0001
+        snakeSections[0].physicsBody?.contactTestBitMask = 15 // 1111
         snakeSections[0].physicsBody?.collisionBitMask = 0
         snakeSections[0].physicsBody?.isDynamic = true
         
         view.showsPhysics = true
+        print(snakeSections[0].position)
     }
  
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
-        snakeSections[0].position = CGPoint(x: snakeSections[0].position.x + 1, y: snakeSections[0].position.y)
+        if !hasContacted {
+            snakeSections[0].position = CGPoint(x: snakeSections[0].position.x - 2, y: snakeSections[0].position.y)
+        } else {
+            snakeSections[0].position = CGPoint(x: frame.maxX, y: snakeSections[0].position.y)
+            hasContacted = false
+        }
+        print(snakeSections[0].position)
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         print("collision detected")
+        
+        hasContacted = true
     }
     
     func initSwipeGestures(_ view: SKView) {
